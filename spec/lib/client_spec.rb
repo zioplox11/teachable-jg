eda7f02105b51f038489b476ef3bc7f8796b5094
Joshua <joshua@mobilecommons.com>
2017-08-14 11:01:04 -0400

    WIP add initial specs for user sign-in / authorization

--- spec/lib/client_spec.rb
+++ spec/lib/client_spec.rb
@@ -55,6 +55,54 @@ describe Teachable::Jg::Client do
           expect(api.send("#{key}")).to eq(@config[key])
         end
       end
+    end
+
+    describe '.authorize' do
+
+      let(:authorized) { {"success"=>true, "login"=>"verified"} }
+      let(:unrecognized) { {"success"=>true, "login"=>"unrecognized or malformed"} }
+
+      let(:invalid) { {"success"=>false, "login"=>"missing or invalid" } }
+
+      context "has user param" do
+        it 'verifies login as successful' do
+          tester = Teachable::Jg::Client.new
+
+
+          VCR.use_cassette('teachable_client_successful') do
+            expect(JSON.parse(tester.authorize(email: "dev-8@example.com", password: "password"))).to eq(authorized)
+          end
+        end
+
+        it 'does not verify login if password incorrect' do
+          tester = Teachable::Jg::Client.new
+
+
+          VCR.use_cassette('teachable_client_successful_malformed1') do
+            expect(JSON.parse(tester.authorize(email: "unregistered@example.com", password: "password"))).to eq(unrecognized)
+          end
+        end
+
+        it 'does not verify login if email is unrecognized' do
+          tester = Teachable::Jg::Client.new
+
+
+          VCR.use_cassette('teachable_client_successful_malformed2') do
+            expect(JSON.parse(tester.authorize(email: "dev-8@example.com", password: "smashword"))).to eq(unrecognized)
+          end
+        end
+      end
+
+      context "without user param" do
+        it 'does not verify login' do
+          tester = Teachable::Jg::Client.new
+
+
+          VCR.use_cassette('teachable_client_unsuccessful') do
+            expect(JSON.parse(tester.authorize(random: "dev-8@example.com", alsorandom: "password"))).to eq(invalid)
+          end
+        end
+      end
 
     end
 
